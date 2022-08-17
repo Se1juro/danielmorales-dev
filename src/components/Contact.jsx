600d9c8c1fadfcf20e220bb064df576b2cd44d87
Daniel Morales <mralejo99@gmail.com>
2022-08-16 23:53:11 -0500

    removed tags dark

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,12 +15,12 @@ const Contact = ()=>{
                 <div className="p-5" style={{maxHeight:"5em",height:"5em"}}>
                   <div className="container-description-project">
                     <a href="https://www.linkedin.com/in/daniel-morales-sevillano/">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Linkedin</h5>
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Linkedin</h5>
                     </a>
                   </div>
                 </div>
 
-                <a href="https://www.linkedin.com/in/daniel-morales-sevillano/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="https://www.linkedin.com/in/daniel-morales-sevillano/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                 Ir al perfil
                   <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
@@ -33,11 +33,11 @@ const Contact = ()=>{
               <div className="p-5">
                 <div className="p-5" style={{maxHeight:"5em",height:"5em"}}>
                   <div className="container-description-project">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Email</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Email</h5>
                   </div>
                 </div>
 
-                <a href="mailto:mralejo99@gmail.com" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="mailto:mralejo99@gmail.com" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                 Enviar email
                   <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
@@ -51,12 +51,12 @@ const Contact = ()=>{
                 <div className="p-5" style={{maxHeight:"5em",height:"5em"}}>
                   <div className="container-description-project">
                     <a href="https://www.linkedin.com/in/daniel-morales-sevillano/">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">GitHub</h5>
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">GitHub</h5>
                     </a>
                   </div>
                 </div>
 
-                <a href="https://www.linkedin.com/in/daniel-morales-sevillano/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="https://www.linkedin.com/in/daniel-morales-sevillano/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                 Ir al perfil
                   <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
